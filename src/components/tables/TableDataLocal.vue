a85f2f70703334ccc229b06a6b04b07e1a99fb81
dang minh <dangminhbk777@gmail.com>
2019-04-23 07:08:55 +0700

    update

---
+++ src/components/tables/TableDataLocal.vue
@@ -0,0 +1,147 @@
+<template>
+  <div class="m_datatable_"></div>
+</template>
+
+<script>
+  export default {
+    name: "TableDataLocal",
+    props: {
+      mData: {
+        type: Array,
+        default: null
+      }
+    },
+    mounted() {
+      let vm = this;
+      let table = $(this.$el);
+      let tableApp =  table.mDatatable({
+        data: {
+          type: 'local',
+          source: vm.mData,
+          pageSize: 10,
+          // serverPaging: true,
+          // serverFiltering: true,
+          // serverSorting: true
+        },
+        processing: true,
+        layout: {
+          theme: 'default',
+          class: '',
+          scroll: true,
+          height: null,
+          minHeight: 100,
+          header: true,
+          footer: false
+        },
+        sortable: true,
+        filterable: false,
+        pagination: true,
+        toolbar: {
+          placement: ['bottom'],
+          items: {
+            pagination: {
+              pageSizeSelect: [1, 5, 10, 20, 30, 50] // display dropdown to select pagination size. -1 is used for "ALl" option
+            },
+          }
+        },
+        columns: [
+          {
+            field: "userHitchhikerId",
+            title: "ID",
+            width: 50,
+            textAlign: 'center',
+            sortable: false,
+          },
+          {
+            field: "username",
+            title: "User Name",
+            sortable: false,
+            width: 100,
+            textAlign: 'left',
+          },
+          {
+            field: "avatar",
+            title: "Avatar",
+            sortable: false,
+            width: 200,
+            textAlign: 'center',
+            template: function (data) {
+              return '<img style="border: solid 1px darkgrey" src="http://localhost:8080/api/images/trip-by-driver/driver__cTICfa_3.png" alt="Image">'
+            }
+          },
+          {
+            field: "phone",
+            title: "Phone",
+            sortable: false,
+            width: 150,
+            textAlign: 'center',
+          },
+          {
+            field: "createdAt",
+            title: "Create At",
+            width: 150,
+            textAlign: 'left',
+            template: function(data) {
+              let timeMilliseconds = data.createdAt;
+              let dateTime = new Date(timeMilliseconds);
+              let options = {
+                year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric'
+              };
+              data.createdAt = dateTime.toLocaleDateString('en', options);
+              return data.createdAt;
+            }
+          },
+          {
+            field: "star",
+            title: "Favourite",
+            sortable: false,
+            width: 100,
+            textAlign: 'center',
+          },
+          {
+            field: "status",
+            title: "Status",
+            width: 250,
+            textAlign: 'center',
+            sortable: false,
+            template: function (data) {
+              let dataId = data.userHitchhikerId;
+              let status = data.status;
+              let temp =
+                  '  <button data-uh-id="'+ dataId +'" class="m-badge--wide btn btn-success btn-sm ___btn-accept">\n' +
+                  '    <span>Accept</span>\n' +
+                  '  </button>\n' +
+                  '  <button data-uh-id="'+ dataId +'" class="m-badge--wide btn btn btn-warning btn-sm ___btn-not-accept">\n' +
+                  '    <span>Not Accept</span>\n' +
+                  '  </button>\n' +
+                  '  <button data-uh-id="'+ dataId +'" class="m-badge--wide btn btn-danger btn-sm ___btn-cancel">\n' +
+                  '    <span>Cancel</span>\n' +
+                  '  </button>\n';
+              return temp;
+            }
+          }
+        ]
+      });
+      tableApp.on('click', '.___btn-accept', function(e) {
+        let btnFirst = $(this);
+        btnFirst.attr("disabled", true);
+        btnFirst.next().attr("disabled", false);
+        btnFirst.next().next().attr("disabled", false);
+      });
+      tableApp.on('click', '.___btn-not-accept', function(e) {
+        let btnBetween = $(this);
+        btnBetween.attr("disabled", true);
+        btnBetween.prev().attr("disabled", false);
+      });
+      tableApp.on('click', '.___btn-cancel', function(e) {
+        let btnLast = $(this);
+        btnLast.attr("disabled", true);
+        btnLast.prev().attr("disabled", false);
+        btnLast.prev().prev().attr("disabled", false);
+      });
+    }
+  }
+</script>
+
+<style scoped>
+</style>
--- src/pages/tripbyhitchhiker/TripByHitchhikerListRegister.vue
+++ src/pages/tripbyhitchhiker/TripByHitchhikerListRegister.vue
@@ -1,10 +1,59 @@
 <template>
-  
+  <div class="row">
+    <div class="m-portlet m-portlet--mobile">
+      <!-- HEAD: BEGIN -->
+      <div class="m-portlet__head">
+        <div class="m-portlet__head-caption">
+          <div class="m-portlet__head-title">
+            <h3 class="m-portlet__head-text">
+              TRIP Data<small>data loaded from remote service</small>
+            </h3>
+          </div>
+        </div>
+      </div>
+      <!-- HEAD: END -->
+      <div class="m-portlet__body">
+        <table-app v-if="mData" :mData="mData"></table-app>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+  import http from '../../services/http-common.js';
+  import Table from '../../components/tables/TableDataLocal.vue';
+
   export default {
-    name: "TripByHitchhikerListRegister"
+    name: "TripByHitchhikerListRegister",
+    components: {
+      'table-app': Table
+    },
+    props: {
+      hitchhikerId: {
+        type: String,
+        default: null
+      }
+    },
+    data: function() {
+      return {
+        mData: null
+      }
+    },
+    methods: {
+      getListRegister: function () {
+        let vm = this;
+        http.get('/trip-by-hitchhiker/register-trip/' + vm.hitchhikerId)
+            .then(response => {
+              vm.mData = JSON.parse(response.data.metadata);
+            })
+            .catch(e => {
+              console.error(e);
+            });
+      }
+    },
+    mounted() {
+      this.getListRegister();
+    }
   }
 </script>
 
