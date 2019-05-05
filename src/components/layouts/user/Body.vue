0206e9f22db63103aad85528b5d0c58f832fbb7b
dang minh <dangminhbk777@gmail.com>
2019-05-05 07:06:58 +0700

    update

--- src/components/layouts/user/Body.vue
+++ src/components/layouts/user/Body.vue
@@ -3,7 +3,7 @@
   <div class="m-grid__item m-grid__item--fluid m-grid m-grid--hor-desktop m-grid--desktop m-body">
     <div class="m-grid__item m-grid__item--fluid m-grid m-grid--ver-desktop m-grid--desktop m-container m-container--responsive m-container--xxl m-container--full-height">
       <div class="m-grid__item m-grid__item--fluid m-wrapper">
-        <sub-header-app-user></sub-header-app-user>
+        <!--<sub-header-app-user></sub-header-app-user>-->
         <content-app-user></content-app-user>
       </div>
     </div>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,13 +1,12 @@
 <template>
-  <div>
-    <div class="row">
+  <div class="_body-home">
+    <div class="row __banner">
       <div class="col-xl-12">
         <!--begin:: Widgets/Blog-->
         <div class="m-portlet m-portlet--bordered-semi m-portlet--full-height  m-portlet--rounded-force">
           <div class="m-portlet__head m-portlet__head--fit">
             <div class="m-portlet__head-caption">
               <div class="m-portlet__head-action">
-                <button type="button" class="btn btn-sm m-btn--pill  btn-brand">Blog</button>
               </div>
             </div>
           </div>
@@ -23,63 +22,77 @@
                 </div>
                 <div class="m-widget19__shadow"></div>
               </div>
-              <!--<div class="m-widget19__content">
-                <div class="m-widget19__header">
-                  <div class="m-widget19__user-img">
-                    <img class="m-widget19__img" src="assets/app/media/img//users/user1.jpg" alt="">
-                  </div>
-                  <div class="m-widget19__info">
-                  <span class="m-widget19__username">
-                      Anna Krox
-                  </span><br>
-                    <span class="m-widget19__time">
-                      UX/UI Designer, Google
-                  </span>
-                  </div>
-                  <div class="m-widget19__stats">
-                  <span class="m-widget19__number m&#45;&#45;font-brand">
-                      18
-                  </span>
-                    <span class="m-widget19__comment">
-                      Comments
-                  </span>
-                  </div>
-                </div>
-                <div class="m-widget19__body">
-                  Lorem Ipsum is simply dummy text of the printing and typesetting industry scrambled it to make text of the printing and typesetting industry scrambled a type specimen book text of the dummy text of the printing printing and typesetting
-                  industry scrambled dummy text of the printing.
-                </div>
-              </div>
-              <div class="m-widget19__action">
-                <button type="button" class="btn m-btn&#45;&#45;pill btn-secondary m-btn m-btn&#45;&#45;hover-brand m-btn&#45;&#45;custom">Read More</button>
-              </div>-->
             </div>
           </div>
         </div>
         <!--end:: Widgets/Blog-->
       </div>
     </div>
-    <div class="row" style="padding-bottom: 40px; padding-top: 60px;">
+    <div class="row" style="padding-bottom: 50px; padding-top: 100px;">
       <div class="col-sm-12 text-center">
         <h1 class="text-uppercase">What is Scoop?</h1>
       </div>
     </div>
     <div class="row justify-content-md-center">
-      <div class="col-sm-3 col-lg-offset-1 col-lg-3 col-sm-pull-5">
-        <div class="wiss-description wiss-description-active" id="wiss-description-0">
-          <strong>Carpooling Service:</strong><br class="hidden-xs"> Scoop brings together co-workers and neighbors who are going the same way.
+      <div class="col-sm-5 col-lg-offset-1 col-lg-3 col-sm-pull-7">
+        <div :class="descriptionCss1" v-on:click="switchShow(1)">
+          <span class="font-weight-bold">Carpooling Service:</span>
+          <br class="hidden-xs"> Scoop brings together co-workers and neighbors who are going the same way.
         </div>
-        <div class="wiss-description wiss-description-inactive" id="wiss-description-1">
-          <strong>Community of Carpoolers:</strong><br class="hidden-xs"> Scoop carpoolers enjoy meeting new people and making the most of their time.
+        <div :class="descriptionCss2" v-on:click="switchShow(2)">
+          <span class="font-weight-bold">Community of Carpoolers:</span>
+          <br class="hidden-xs"> Scoop carpoolers enjoy meeting new people and making the most of their time.
         </div>
-        <div class="wiss-description wiss-description-inactive" id="wiss-description-2">
-          <strong>Enterprise Solution:</strong><br class="hidden-xs"> Scoop partners with competitive employers to enrich the lives of their employees.
+        <div :class="descriptionCss3" v-on:click="switchShow(3)">
+          <span class="font-weight-bold"> Enterprise Solution:</span>
+          <br class="hidden-xs"> Scoop partners with competitive employers to enrich the lives of their employees.
         </div>
       </div>
-      <div class="col-sm-5 col-sm-push-3 text-center">
-        <img src="https://www.takescoop.com/static/images/what-is-scoop/carpooling-service.png" id="wiss-image-0" class="wiss-image" alt="Carpooling service">
-        <img src="https://www.takescoop.com/static/images/what-is-scoop/community-of-carpoolers.png" id="wiss-image-1" class="wiss-image init-hidden" alt="Community of carpoolers">
-        <img src="https://www.takescoop.com/static/images/what-is-scoop/enterprise-solution.png" id="wiss-image-2" class="wiss-image init-hidden" alt="An enterprise solution">
+      <div class="col-sm-7 col-sm-push-5 text-center">
+        <img src="https://www.takescoop.com/static/images/what-is-scoop/carpooling-service.png" :class="imageCss1" alt="Carpooling service">
+        <img src="https://www.takescoop.com/static/images/what-is-scoop/community-of-carpoolers.png" :class="imageCss2" alt="Community of carpoolers">
+        <img src="https://www.takescoop.com/static/images/what-is-scoop/enterprise-solution.png" :class="imageCss3" alt="An enterprise solution">
+      </div>
+    </div>
+    <div class="row" style="padding-bottom: 50px; padding-top: 100px; background-color: #F7F7F7">
+      <div class="col-sm-12 text-center">
+        <h1 class="text-uppercase">Here’s how it works</h1>
+      </div>
+    </div>
+    <div class="row justify-content-md-center" style="background-color: #F7F7F7">
+      <div class="col-xs-12 col-sm-5 col-sm-offset-2">
+        <img src="https://www.takescoop.com/static/images/how-it-works/see-your-community.png" class="home-image-md" alt="">
+      </div>
+      <div class="col-xs-12 col-sm-5">
+        <h2>Be Part of the Community</h2>
+        <p class="home-description-part">Download and meet Scoop carpoolers who are heading the same way. Get free Carpool Credits when you choose Scoop.</p>
+      </div>
+    </div>
+    <div class="row justify-content-md-center" style="background-color: #F7F7F7; padding-top: 50px">
+      <div class="col-xs-12 col-sm-5" style="padding-top: 50px">
+        <h2>Schedule a Carpool</h2>
+        <p class="home-description-part">Separate AM and PM trips to fit your unpredictable work schedule. Ride or drive to meet your daily needs.</p>
+      </div>
+      <div class="col-xs-12 col-sm-5 col-sm-offset-2">
+        <img src="https://www.takescoop.com/static/images/how-it-works/request-a-trip.png" class="home-image-md" alt="">
+      </div>
+    </div>
+    <div class="row justify-content-md-center" style="background-color: #F7F7F7; padding-top: 50px">
+      <div class="col-xs-12 col-sm-5 col-sm-offset-2">
+        <img src="https://www.takescoop.com/static/images/how-it-works/get-matched.png" class="home-image-md" alt="">
+      </div>
+      <div class="col-xs-12 col-sm-5" style="padding-top: 50px">
+        <h2>Get Matched</h2>
+        <p class="home-description-part">Scoop’s algorithm identifies the most efficient trip based on the fastest route, nearby carpoolers, carpool lanes, and more.</p>
+      </div>
+    </div>
+    <div class="row justify-content-md-center" style="background-color: #F7F7F7; padding-top: 50px">
+      <div class="col-xs-12 col-sm-5" style="padding-top: 50px">
+        <h2>Enjoy a Smooth Commute</h2>
+        <p class="home-description-part">Meet new people, have more energy every day, and make the most of your time.</p>
+      </div>
+      <div class="col-xs-12 col-sm-5 col-sm-offset-2">
+        <img src="https://www.takescoop.com/static/images/how-it-works/enjoy.png" class="home-image-md" alt="">
       </div>
     </div>
   </div>
@@ -88,84 +101,65 @@
 <script>
   export default {
     name: "Home",
-    mounted() {
-      let element0 = $('#wiss-description-0');
-      let element1 = $('#wiss-description-1');
-      let element2 = $('#wiss-description-2');
-
-      let image0 = $('#wiss-image-0');
-      let image1 = $('#wiss-image-1');
-      let image2 = $('#wiss-image-2');
-
-      element0.on('click', function () {
-        if (element0.hasClass('wiss-description-inactive')) {
-          element0.removeClass('wiss-description-inactive');
-          element0.addClass('wiss-description-active');
-
-          image0.removeClass('init-hidden');
+    data() {
+      return {
+        descriptionCss1: 'home-description home-description-active',
+        descriptionCss2: 'home-description home-description-inactive',
+        descriptionCss3: 'home-description home-description-inactive',
+        imageCss1: 'home-image',
+        imageCss2: 'home-image init-hidden',
+        imageCss3: 'home-image init-hidden',
+        descriptionActive: 'home-description home-description-active',
+        descriptionInActive: 'home-description home-description-inactive',
+        image: 'home-image',
+        imageHidden: 'home-image init-hidden',
+      }
+    },
+    methods: {
+      switchShow: function (id) {
+        let self = this;
+        if (id === 1) {
+          // Set css for description
+          self.descriptionCss1 = self.descriptionActive;
+          self.descriptionCss2 = self.descriptionInActive;
+          self.descriptionCss3 = self.descriptionInActive;
+          // Set css image
+          self.imageCss1 = self.image;
+          self.imageCss2 = self.imageHidden;
+          self.imageCss3 = self.imageHidden;
+        } else if (id === 2) {
+          // Set css for description
+          self.descriptionCss1 = self.descriptionInActive;
+          self.descriptionCss2 = self.descriptionActive;
+          self.descriptionCss3 = self.descriptionInActive;
+          // Set css image
+          self.imageCss1 = self.imageHidden;
+          self.imageCss2 = self.image;
+          self.imageCss3 = self.imageHidden;
+        } else if (id === 3) {
+          // Set css for description
+          self.descriptionCss1 = self.descriptionInActive;
+          self.descriptionCss2 = self.descriptionInActive;
+          self.descriptionCss3 = self.descriptionActive;
+          // Set css image
+          self.imageCss1 = self.imageHidden;
+          self.imageCss2 = self.imageHidden;
+          self.imageCss3 = self.image;
         }
-        if (element1.hasClass('wiss-description-active')) {
-          element1.removeClass('wiss-description-active');
-          element1.addClass('wiss-description-inactive');
-
-          image1.addClass('init-hidden');
-        }
-        if (element2.hasClass('wiss-description-active')) {
-          element2.removeClass('wiss-description-active');
-          element2.addClass('wiss-description-inactive');
-
-          image2.addClass('init-hidden');
-        }
-      });
-      element1.on('click', function () {
-        if (element1.hasClass('wiss-description-inactive')) {
-          element1.removeClass('wiss-description-inactive');
-          element1.addClass('wiss-description-active');
-
-          image1.removeClass('init-hidden');
-        }
-        if (element2.hasClass('wiss-description-active')) {
-          element2.removeClass('wiss-description-active');
-          element2.addClass('wiss-description-inactive');
-
-          image2.addClass('init-hidden');
-        }
-        if (element0.hasClass('wiss-description-active')) {
-          element0.removeClass('wiss-description-active');
-          element0.addClass('wiss-description-inactive');
-
-          image0.addClass('init-hidden');
-        }
-      });
-      element2.on('click', function () {
-        if (element2.hasClass('wiss-description-inactive')) {
-          element2.removeClass('wiss-description-inactive');
-          element2.addClass('wiss-description-active');
-
-          image2.removeClass('init-hidden');
-        }
-        if (element0.hasClass('wiss-description-active')) {
-          element0.removeClass('wiss-description-active');
-          element0.addClass('wiss-description-inactive');
-
-          image0.addClass('init-hidden');
-        }
-        if (element1.hasClass('wiss-description-active')) {
-          element1.removeClass('wiss-description-active');
-          element1.addClass('wiss-description-inactive');
-
-          image1.addClass('init-hidden');
-        }
-      });
+      }
     }
   }
 </script>
 
 <style scoped>
-  .wiss-description-active {
+  .__banner {
+    margin-right: -80px;
+    margin-left: -80px;
+  }
+  .home-description-active {
     border: 2px solid #ffad4e;
   }
-  .wiss-description {
+  .home-description {
     padding: 20px 20px;
     margin-bottom: 30px;
     font-size: 15px;
@@ -174,11 +168,11 @@
     cursor: pointer;
     transition: .3s all;
   }
-  .wiss-description-inactive {
+  .home-description-inactive {
     border: 2px solid #dadddd;
     color: #9fa7a6;
   }
-  .wiss-description {
+  .home-description {
     padding: 20px 20px;
     margin-bottom: 30px;
     font-size: 15px;
@@ -187,14 +181,22 @@
     cursor: pointer;
     transition: .3s all;
   }
-  .wiss-image {
+  .home-image {
     width: 500px;
     height: 450px;
+  }
+  .home-image-md {
+    width: 330px;
+    height: 330px;
   }
   .init-hidden {
     display: none;
   }
   .m-portlet .m-portlet__body {
     padding: 0;
+  }
+  .home-description-part {
+    font-size: 16px;
+    line-height: 1.9;
   }
 </style>
